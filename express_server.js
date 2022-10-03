174e0cb5259aa4fde66fe67e0dcac53c5dcb79e8
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-03 18:41:05 -0400

    adding a post method for users to submit form to urls/new

--- express_server.js
+++ express_server.js
@@ -9,10 +9,15 @@ const PORT = 8080;
 ////setting view engine
 app.set("view engine", "ejs");
 
+///middleware to translate request body
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+///defining routing
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,21 +32,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req);
+  res.send("ok");
+});
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new"
-  );
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
+  console.log(req);
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+//listen at port 8080
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
