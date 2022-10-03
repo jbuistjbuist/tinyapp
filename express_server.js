9e41a0ba3aaec768c25b041a4af851c42579aa17
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-03 19:43:28 -0400

    Update expressserver.js to respond with a redirect from /urls to /urls/:id

--- express_server.js
+++ express_server.js
@@ -65,6 +65,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 
 
 //listen at port 8080
