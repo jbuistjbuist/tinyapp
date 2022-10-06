c952dd134cffbc9a3edbc6f66276c5e72bde7a1b
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-06 14:25:12 -0400

    replace cookie-parser package with cookie-session package

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcryptjs");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const {urlDatabase, users} = require('./sitedata');
 const {generateRandomString, findUserEmail, urlsForUser, canEditDelete} = require('./helper_functions');
 
@@ -15,7 +15,10 @@ app.set("view engine", "ejs");
 
 ///middleware to translate request body and handle cookies
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: 
+})
 
 ////  DEFINING ROUTING   /////
 
